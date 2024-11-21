074fa01b60f4a29d3a96d7cec3c7d7476908b9bd
Kacper Domański <116498271+KDoman@users.noreply.github.com>
2024-11-21 22:52:37 +0100

    update Cards in list

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,8 @@
 import "./Card.scss";
 import { Link } from "react-router-dom";
+import PERSON_ICON from "../assets/Person.svg";
+import HOME_ICON from "../assets/Home.svg";
+import STAR_ICON from "../assets/Star.svg";
 
 export const Card = ({ item }) => {
   return (
@@ -9,12 +12,29 @@ export const Card = ({ item }) => {
       </div>
       <div className="text_container">
         <div className="text_name_container">
+          <img
+            className="svg_icon"
+            src={PERSON_ICON}
+            alt="Obrazek w formacie SVG reprezentujące osobę"
+          />
           <p>{item.name}</p>
           <p>{item.surname}</p>
-          <span className="text_city">{item.city}</span>
+          <div className="text_city_container">
+            <img
+              className="svg_icon"
+              src={HOME_ICON}
+              alt="Obrazek w formacie SVG przedstawiający domek"
+            />
+            <span className="text_city">{item.city}</span>
+          </div>
         </div>
         <div className="text_rating">
-          <p>
+          <img
+            className="svg_icon"
+            src={STAR_ICON}
+            alt="Obrazek w formacie SVG przedstawiający gwiazdę"
+          />
+          <p className="text_rating_p">
             {(
               item.rating.reduce((acc, val) => acc + val) / item.rating.length
             ).toFixed(2)}
