425fd5cdf796cf2a03f22aaed8b5912accbe9761
Doman <itdoman1999@gmail.com>
2024-12-04 14:44:06 +0100

    working on chat

--- src/routes/Messages/Messages.jsx
+++ src/routes/Messages/Messages.jsx
@@ -2,11 +2,21 @@ import { HistoryChat } from "../../components/HistoryChat";
 import "./Messages.scss";
 import { listData } from "../../data/data";
 import { Label } from "../../components/Label";
+import HOME_LOGO from "../../assets/Home.svg";
+import BACK_ICON from "../../assets/Back.svg";
+import { Link } from "react-router-dom";
 
 export const Messages = () => {
   return (
     <>
-      <h1>Historia wiadomości</h1>
+      <div className="messages_page_entry_div">
+        <Link to={`/list`}>
+          <div className="back_icon_container">
+            <img src={BACK_ICON} alt="" className="back_icon" />
+          </div>
+        </Link>
+        <h1>Historia wiadomości</h1>
+      </div>
       <div className="messages_page_container">
         <div className="left_search_filter">
           <input
@@ -32,13 +42,21 @@ export const Messages = () => {
           <p>
             {listData[0].name} {listData[0].surname}
           </p>
+          <div className="right_profile_info_city">
+            <img src={HOME_LOGO} alt="" />
+            <p>{listData[0].city}</p>
+          </div>
         </div>
         <div className="right_more_profile_info">
+          <h2>Informacje</h2>
+          <p>Odznaki</p>
           <div className="right_more_profile_info_labels">
             {listData[0].labels.map((label) => (
               <Label key={label}>{label}</Label>
             ))}
           </div>
+          <div className="right_more_prfile_info_review"></div>
+          <div className="right_more_prfile_info_review_comment"></div>
         </div>
       </div>
     </>
