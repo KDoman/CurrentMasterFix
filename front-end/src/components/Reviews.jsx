5bb4ea985bef5c366acf941e15f7d7110a525bac
Doman <itdoman1999@gmail.com>
2024-12-29 23:11:17 +0100

    quick small UI fixes after review system

--- front-end/src/components/Reviews.jsx
+++ front-end/src/components/Reviews.jsx
@@ -5,18 +5,24 @@ export const Reviews = ({ name, rating }) => {
   return (
     <div className="reviews_container">
       {rating.length ? (
-        <h3>Historia fachowca {name}</h3>
+        <h3>
+          Opinie o fachowcu <span className="review_span_name">{name}</span>
+        </h3>
       ) : (
-        "Ten fachowiec nie ma jeszcze wystawionych ocen"
+        "Ten fachowiec nie ma jeszcze wystawionych ocen."
       )}
 
       {rating.map((rate, id) => (
         <div className="review" key={id}>
+          <p>
+            <i>{rate.name}</i>
+          </p>
           <p className="review_mark">
             <span className="review_counter">{`#${id + 1}`}</span>
             {rate.mark}
             <img src={STAR_LOGO} alt="" className="review_star" />
           </p>
+
           <p>{rate.message}</p>
         </div>
       ))}
