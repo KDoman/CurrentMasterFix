5e7059e55d05d7bf1e59c890fd47047a4ef5b084
Kacper Domański <itdoman1999@gmail.com>
2025-02-22 23:00:57 +0100

    url fix

--- front-end/src/api_utils/api.js
+++ front-end/src/api_utils/api.js
@@ -1,5 +1,4 @@
-// const BASE_URL = "https://masterfix-backend-pbgj.onrender.com/api";
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://masterfix-backend-pbgj.onrender.com/api";
 
 
 
